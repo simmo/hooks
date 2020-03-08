6863772a6ed8501df5a2e6c098dbe968992d88e9
Mike Simmonds <mike@simmo.me>
2020-03-08 12:23:16 +0000

    Adding repo readme script

---
+++ scripts/generateReadme.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires, no-undef */
+
+const { readDirAsync, writeFileAsync } = require('./utils')
+
+const cwd = process.cwd()
+
+const template = `# React Hooks
+A modular collection of React hooks.
+
+🦄 TypeScript support • 🐐 Fully tested • 👾 Server rendering compatible
+
+![Travis](https://img.shields.io/travis/com/simmo/hooks?style=flat-square)
+
+## Packages
+
+See ./packages/* for individual package installation details.
+
+| Package | Version | Description |
+| ------- | ------- | ----------- |`
+
+async function run() {
+  const packageNames = await readDirAsync('./packages', {
+    withFileTypes: true,
+  })
+
+  const packages = packageNames
+    .filter(item => item.isDirectory())
+    .map(item => {
+      const pkg = require(`${cwd}/packages/${item.name}/package.json`)
+      const [scope, name] = pkg.name.split('/')
+      const npmBadge = `![npm](https://img.shields.io/npm/v/${scope.replace(
+        '@',
+        ''
+      )}/${name}?style=flat-square)`
+
+      return `\n| [${pkg.name}](https://github.com/simmo/hooks/tree/master/packages/${item.name}) | ${npmBadge} | ${pkg.description} |`
+    })
+
+  await writeFileAsync('README.md', `${template}${packages}`)
+}
+
+run()
