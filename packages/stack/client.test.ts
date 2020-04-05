c37105f22a0916478d9d33a2fa10e90a5daaec2b
Mike Simmonds <mike@simmo.me>
2020-04-05 18:34:45 +0100

    Fixing conflicts

--- packages/stack/client.test.ts
+++ packages/stack/client.test.ts
@@ -1,44 +1,19 @@
 import { renderHook } from '@testing-library/react-hooks'
 import useStack from '.'
+import Stack from './Stack'
 
 describe('useStack', () => {
   test('initalises with an empty stack', () => {
     const { result } = renderHook(() => useStack())
 
+    expect(result.current).toBeInstanceOf(Stack)
     expect(result.current.size).toBe(0)
   })
 
   test('initalises with a populated stack', () => {
     const { result } = renderHook(() => useStack(['a', 'b', 'c']))
 
-    expect(result.current.size).toBe(3)
-  })
-
-  test('removes and returns the item from the top of the stack', () => {
-    const { result } = renderHook(() => useStack(['a', 'b', 'c']))
-
-    expect(result.current.size).toBe(3)
-    expect(result.current.pop()).toBe('c')
-    expect(result.current.size).toBe(2)
-    expect(result.current.peek()).toBe('b')
-  })
-
-  test('adds the item to the top of the stack', () => {
-    const { result } = renderHook(() => useStack(['a', 'b', 'c']))
-
-    expect(result.current.size).toBe(3)
-
-    result.current.push('d')
-
-    expect(result.current.size).toBe(4)
-    expect(result.current.peek()).toBe('d')
-  })
-
-  test('returns the item at the top of the stack', () => {
-    const { result } = renderHook(() => useStack(['a', 'b', 'c']))
-
-    expect(result.current.size).toBe(3)
-    expect(result.current.peek()).toBe('c')
+    expect(result.current).toBeInstanceOf(Stack)
     expect(result.current.size).toBe(3)
   })
 })
--- packages/stack/server.test.ts
+++ packages/stack/server.test.ts
@@ -4,45 +4,20 @@
 
 import renderHookServer from '../../utils/renderHookServer'
 import useStack from '.'
+import Stack from './Stack'
 
 describe('useStack', () => {
   test('initalises with an empty stack', () => {
     const result = renderHookServer(() => useStack())
 
+    expect(result).toBeInstanceOf(Stack)
     expect(result.size).toBe(0)
   })
 
   test('initalises with a populated stack', () => {
     const result = renderHookServer(() => useStack(['a', 'b', 'c']))
 
-    expect(result.size).toBe(3)
-  })
-
-  test('removes and returns the item from the top of the stack', () => {
-    const result = renderHookServer(() => useStack(['a', 'b', 'c']))
-
-    expect(result.size).toBe(3)
-    expect(result.pop()).toBe('c')
-    expect(result.size).toBe(2)
-    expect(result.peek()).toBe('b')
-  })
-
-  test('adds the item to the top of the stack', () => {
-    const result = renderHookServer(() => useStack(['a', 'b', 'c']))
-
-    expect(result.size).toBe(3)
-
-    result.push('d')
-
-    expect(result.size).toBe(4)
-    expect(result.peek()).toBe('d')
-  })
-
-  test('returns the item at the top of the stack', () => {
-    const result = renderHookServer(() => useStack(['a', 'b', 'c']))
-
-    expect(result.size).toBe(3)
-    expect(result.peek()).toBe('c')
+    expect(result).toBeInstanceOf(Stack)
     expect(result.size).toBe(3)
   })
 })
