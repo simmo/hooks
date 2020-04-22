c735a37d82273af32e2b519c1ac60c6cf8c9225c
Matteo Pieroni <matteo.pieroni@zonedigital.com>
2020-04-22 12:04:23 +0100

    Added error handling

--- scripts/tasks/gen-package-readme/index.js
+++ scripts/tasks/gen-package-readme/index.js
@@ -44,9 +44,177 @@ npm i ${name}
 \`\`\``,
 ]
 
+const generatePackagesScript = (packages, collectError = () => {}) => packages.map(package => ({
+  title: package,
+  task: () => {
+    const packagePath = `${cwd}/packages/${package}`
+    const { name, description } = require(`${packagePath}/package.json`)
+    const readme = createReadme({ name, description })
+
+    return new Listr([
+      {
+        title: 'Process TypeScript',
+        task: () => {
+          const file = `${packagePath}/index.ts`
+          const program = ts.createProgram([file], { allowJs: true })
+          // const checker = program.getTypeChecker()
+          const sourceFile = program.getSourceFile(file)
+          let hasExport = false
+
+          ts.forEachChild(sourceFile, node => {
+            if (ts.isFunctionDeclaration(node)) {
+              const isExport = node.modifiers.find(
+                modifier => modifier.kind === ts.SyntaxKind.ExportKeyword
+              )
+
+              if (isExport) {
+                if (!hasExport) {
+                  readme.push('## Usage')
+                }
+
+                // const symbol = checker.getSymbolAtLocation(node.name)
+                // console.log(symbol.valueDeclaration)
+                // console.log({
+                //   name: symbol.getName(),
+                //   documentation: ts.displayPartsToString(
+                //     symbol.getDocumentationComment(checker)
+                //   ),
+                //   type: checker.typeToString(
+                //     checker.getTypeOfSymbolAtLocation(
+                //       symbol,
+                //       symbol.valueDeclaration
+                //     )
+                //   ),
+                // })
+
+                hasExport = true
+
+                readme.push(`### ${node.name.text}`)
+
+                const jsDocParamComments = node.jsDoc
+                  ? node.jsDoc.reduce(
+                      (obj, doc) => {
+                        if (doc.tags.length) {
+                          doc.tags.forEach(tag => {
+                            if (ts.isJSDocParameterTag(tag)) {
+                              obj.parameters[tag.name.text] = tag.comment
+                            }
+
+                            if (ts.isJSDocReturnTag(tag)) {
+                              obj.return = tag.comment
+                            }
+                          })
+                        }
+
+                        return obj
+                      },
+                      { parameters: {}, return: null }
+                    )
+                  : { parameters: {}, return: null }
+
+                const returnType = node.type
+                  ? `: ${node.type.getText(sourceFile)}`
+                  : ''
+
+                readme.push(
+                  `\`\`\`ts\n${node.name.text}(${
+                    node.parameters.length
+                      ? node.parameters
+                          .map(param => param.getText(sourceFile))
+                          .join(', ')
+                      : ''
+                  })${returnType}\n\`\`\``
+                )
+
+                if (node.parameters.length) {
+                  readme.push(`#### Parameters`)
+
+                  // console.log(node.declarationList)
+
+                  node.parameters.forEach(param => {
+                    readme.push(`##### \`${param.getText(sourceFile)}\``)
+
+                    // console.log(JSON.stringify(param, null, 2))
+
+                    // if (ts.isTypeReferenceNode(param.type)) {
+                    //   console.log(param.declarationList)
+                    // }
+
+                    const jsDocParamComment =
+                      jsDocParamComments.parameters[param.name.text]
+
+                    if (jsDocParamComment) {
+                      readme.push(jsDocParamComment)
+                    }
+                  })
+                }
+
+                if (jsDocParamComments.return) {
+                  readme.push(`#### Return`)
+                  readme.push(jsDocParamComments.return)
+                }
+              }
+            }
+          })
+        },
+      },
+      {
+        title: 'Save',
+        task: async () => {
+          try {
+            const content = prettier.format(`${readme.join('\n\n')}\n`, {
+              parser: 'markdown',
+            })
+
+            return await writeFileAsync(`${packagePath}/README.md`, content)
+          } catch(e) {
+            collectError({ package, step: 'Save', verbose: e })
+            // we throw the error again to make sure that Listr shows the failed step in the console
+            throw new Error(e)
+          }
+        },
+      },
+      {
+        title: 'Generate story file',
+        skip: async () => {
+          try {
+            await accessFileAsync(`${packagePath}/StoryComponent.tsx`)
+
+            // to execute the step we need to return a falsy value
+            return false
+          } catch (e) {
+            // if accessFileAsync errors there is no file, so we can skip the step
+            // to skip it we need to return a truthy value
+            return 'No story component, skipping story file'
+          }
+        },
+        task: async () => {
+          try {
+            createStory(readme, { name })
+
+            const content = prettier.format(`${readme.join('\n\n')}\n`, {
+              parser: 'markdown',
+            })
+
+            return await writeFileAsync(
+              `${packagePath}/README.story.mdx`,
+              content
+            )
+          } catch(e) {
+            collectError({ package, step: 'Generate story file', verbose: e })
+            // we throw the error again to make sure that Listr shows the failed step in the console
+            throw new Error(e)
+          }
+        },
+      },
+    ])
+  },
+}))
+
 const script = createScript({
   name: 'Generate package README',
   task: async () => {
+    const errors = []
     const choices = await getAllPackages()
     const { packages } = await prompt([
       {
@@ -62,164 +230,42 @@ const script = createScript({
     ])
 
     const tasks = new Listr(
-      packages.map(package => ({
-        title: package,
-        task: () => {
-          const packagePath = `${cwd}/packages/${package}`
-          const { name, description } = require(`${packagePath}/package.json`)
-          const readme = createReadme({ name, description })
-
-          return new Listr([
-            {
-              title: 'Process TypeScript',
-              task: () => {
-                const file = `${packagePath}/index.ts`
-                const program = ts.createProgram([file], { allowJs: true })
-                // const checker = program.getTypeChecker()
-                const sourceFile = program.getSourceFile(file)
-                let hasExport = false
-
-                ts.forEachChild(sourceFile, node => {
-                  if (ts.isFunctionDeclaration(node)) {
-                    const isExport = node.modifiers.find(
-                      modifier => modifier.kind === ts.SyntaxKind.ExportKeyword
-                    )
-
-                    if (isExport) {
-                      if (!hasExport) {
-                        readme.push('## Usage')
-                      }
-
-                      // const symbol = checker.getSymbolAtLocation(node.name)
-                      // console.log(symbol.valueDeclaration)
-                      // console.log({
-                      //   name: symbol.getName(),
-                      //   documentation: ts.displayPartsToString(
-                      //     symbol.getDocumentationComment(checker)
-                      //   ),
-                      //   type: checker.typeToString(
-                      //     checker.getTypeOfSymbolAtLocation(
-                      //       symbol,
-                      //       symbol.valueDeclaration
-                      //     )
-                      //   ),
-                      // })
-
-                      hasExport = true
-
-                      readme.push(`### ${node.name.text}`)
-
-                      const jsDocParamComments = node.jsDoc
-                        ? node.jsDoc.reduce(
-                            (obj, doc) => {
-                              if (doc.tags.length) {
-                                doc.tags.forEach(tag => {
-                                  if (ts.isJSDocParameterTag(tag)) {
-                                    obj.parameters[tag.name.text] = tag.comment
-                                  }
-
-                                  if (ts.isJSDocReturnTag(tag)) {
-                                    obj.return = tag.comment
-                                  }
-                                })
-                              }
-
-                              return obj
-                            },
-                            { parameters: {}, return: null }
-                          )
-                        : { parameters: {}, return: null }
-
-                      const returnType = node.type
-                        ? `: ${node.type.getText(sourceFile)}`
-                        : ''
-
-                      readme.push(
-                        `\`\`\`ts\n${node.name.text}(${
-                          node.parameters.length
-                            ? node.parameters
-                                .map(param => param.getText(sourceFile))
-                                .join(', ')
-                            : ''
-                        })${returnType}\n\`\`\``
-                      )
-
-                      if (node.parameters.length) {
-                        readme.push(`#### Parameters`)
-
-                        // console.log(node.declarationList)
-
-                        node.parameters.forEach(param => {
-                          readme.push(`##### \`${param.getText(sourceFile)}\``)
-
-                          // console.log(JSON.stringify(param, null, 2))
-
-                          // if (ts.isTypeReferenceNode(param.type)) {
-                          //   console.log(param.declarationList)
-                          // }
-
-                          const jsDocParamComment =
-                            jsDocParamComments.parameters[param.name.text]
-
-                          if (jsDocParamComment) {
-                            readme.push(jsDocParamComment)
-                          }
-                        })
-                      }
-
-                      if (jsDocParamComments.return) {
-                        readme.push(`#### Return`)
-                        readme.push(jsDocParamComments.return)
-                      }
-                    }
-                  }
-                })
-              },
-            },
-            {
-              title: 'Save',
-              task: async () => {
-                const content = prettier.format(`${readme.join('\n\n')}\n`, {
-                  parser: 'markdown',
-                })
-
-                return await writeFileAsync(`${packagePath}/README.md`, content)
-              },
-            },
-            {
-              title: 'Generate story file',
-              skip: async () => {
-                try {
-                  await accessFileAsync(`${packagePath}/StoryComponent.tsx`)
-
-                  // to execute the step we need to return a falsy value
-                  return false
-                } catch (e) {
-                  // if accessFileAsync errors there is no file, so we can skip the step
-                  // to skip it we need to return a truthy value
-                  return 'No story component, skipping story file'
-                }
-              },
-              task: async () => {
-                createStory(readme, { name })
-
-                const content = prettier.format(`${readme.join('\n\n')}\n`, {
-                  parser: 'markdown',
-                })
-
-                return await writeFileAsync(
-                  `${packagePath}/README.story.mdx`,
-                  content
-                )
-              },
-            },
-          ])
-        },
-      })),
-      { concurrent: 2, exitOnError: false }
+      generatePackagesScript(packages, (e) => errors.push(e)),
+      {
+        concurrent: 2,
+        exitOnError: false
+      }
     )
 
-    return await tasks.run()
+    try {
+      await tasks.run();
+    } catch(e) {
+      if (errors.length > 0) {
+        const message =
+          'The following packages could not be built:' +
+          errors.reduce((agg, error, i) => `${agg} ${error.package}${i === errors.length - 1 ? '.' : ','}`, '') +
+          '\nSelect any packages you want to retry.'
+
+        const { retryPackages } = await prompt([
+          {
+            name: 'retryPackages',
+            message,
+            type: 'checkbox',
+            choices: errors.map(error => error.package),
+          },
+        ])
+
+        const errorTasks = await new Listr(
+          generatePackagesScript(retryPackages, error => console.log(error.verbose)),
+          {
+            concurrent: 2,
+            exitOnError: false,
+          }
+        )
+
+        return await errorTasks.run()
+      }
+    }
   },
 })
 
